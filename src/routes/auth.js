1f4b4bcf8149d4803fdb0cfb62dc24213bcc0c9b
DukTai <nguyenductai.12a2tqk@gmail.com>
2026-03-05 11:00:00 +0700

    Done auth

---
+++ src/routes/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../controllers/authController");
+const { checkAuth } = require("../middleware/auth");
+
+router.post("/register", auth.register);
+router.post("/login", auth.login);
+router.post("/forgot-password", auth.forgotPassword);
+router.post("/reset-password", auth.resetPassword);
+
+// Protected change password
+router.post("/change-password", checkAuth, auth.changePassword);
+
+module.exports = router;
